cc71b13c31cc1927f8f3b83abcfc84ca740dcddc
Ronnylrsd <ronnylrsd@gmail.com>
2022-09-24 09:12:37 -0300

    Feat: create avenger

---
+++ Jogo da Velha/src/jogadores/Avenger.java
@@ -0,0 +1,16 @@
+package jogadores;
+
+public class Avenger extends Jogador{
+
+    public Avenger(String nome) {
+        super(nome);
+        //TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public int[] jogar(int[][] tabuleiro) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+}
